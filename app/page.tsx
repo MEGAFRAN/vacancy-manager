6ce2f78ab42f7f656b7a7cc18d02ce3cbe829908
Francisco cruz <megafran@msn.com>
2024-03-30 09:37:14 +0100

    feature: added title and metadata

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div>
       <main>
+        <h1 style={{ textAlign: "center" }}>Track selection process steps, for tech jobs</h1>
         <Dashboard />
       </main>
 
